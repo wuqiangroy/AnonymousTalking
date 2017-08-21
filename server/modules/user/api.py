1baaa2fed47cc0872a53a70f15b2caecddbd0c8b
wuqiangroy <wuqiangroy@live.com>
2017-08-21 22:58:12 +0800

    *_*

--- server/modules/user/api.py
+++ server/modules/user/api.py
@@ -4,30 +4,27 @@
 from flask import jsonify, request
 from flask.views import MethodView
 
+from . import user
 from .controller import *
 from ..const import Const, Msg
 from .model import User
 
 
-class BaseView(MethodView):
-    def dispatch_request(self, *args, **kwargs):
-        status, data = super(BaseView, self).dispatch_request(*args, **kwargs)
-        return jsonify({
-            "code": status,
-            "msg": Msg.msg[status],
-            "data": data
-        })
+def make_response(status=1200, data=None):
+    return jsonify({
+        "code": status,
+        "msg": Msg.msg[status],
+        "data": data
+    })
 
 
-class Login(BaseView):
-    """login view"""
-
-    def post(self):
-        form = LoginForm(request.form)
-        if not form:
-            return Const.MISSPARAM, None
-        phone = form.get("phone")
-        password = form.get("password")
-        user = User.objects(phone=phone).first()
-        if not user:
-            return Const.NO_REGISTER, None
+@user.route("/login", methods=["POST"])
+def login():
+    form = LoginForm(request.form)
+    if not form:
+        return make_response(status=Const.MISSPARAM, data=None)
+    phone = form.get("phone")
+    password = form.get("password")
+    user = User.objects(phone=phone).first()
+    if not user:
+        return make_response(status=Const.NO_REGISTER, data=None)
